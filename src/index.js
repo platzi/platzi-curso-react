4c82d76787be5ac32939ece537b1892b85f3623b
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-05 15:00:34 -0500

    Aula12

--- src/index.js
+++ src/index.js
@@ -1,48 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const PlatziFuncional = props => {
-  const [texto, setTexto] = React.useState('Oi');
-  const [pessoa, setPessoa] = React.useState({
-    id: 1,
-    nome: 'Luís'
+const Contador = () => {
+  const [valor, setValor] = React.useState(0);
+
+  React.useEffect(() => {
+    const timerID = setInterval(() => {
+      setValor(v => v + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timerID);
+    };
   });
+
   return (
     <div>
-      <h3 className="platzi">
-        {texto} {props.nome}
-      </h3>
-      <button
-        onClick={() => {
-          setTexto(textoAnterior =>
-            textoAnterior === 'Oi'
-              ? 'Tchau'
-              : 'Oi'
-          );
-        }}
-      >
-        Mudar estado
-      </button>
-      <p>
-        {pessoa.id} - {pessoa.nome}
-      </p>
-      <button
-        onClick={() => {
-          setPessoa(pessoaAnterior => ({
-            id: pessoaAnterior.id + 1,
-            nome: pessoaAnterior.nome
-          }));
-        }}
-      >
-        Mudar estado da pessoa
-      </button>
+      <h1>{valor}</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  const [
+    mostrarContador,
+    setMostrarContador
+  ] = React.useState(true);
+  const toggleMostrador = () => {
+    setMostrarContador(ant => !ant);
+  };
+  if (!mostrarContador) {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          checked={mostrarContador}
+          onChange={toggleMostrador}
+        />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={mostrarContador}
+        onChange={toggleMostrador}
+      />
+      <Contador />
     </div>
   );
 };
 
 ReactDOM.render(
-  <div>
-    <PlatziFuncional nome="Maria" />
-  </div>,
+  <App />,
   document.getElementById('root')
 );
