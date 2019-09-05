dec81790295d7d1c6ba3b4e1ec6e21d931848578
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-05 11:59:44 -0500

    Aula10

--- src/index.js
+++ src/index.js
@@ -1,74 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-class Resultado extends React.Component {
-  render() {
-    return <h1>{this.props.resultado}</h1>;
+const pessoas = [
+  {
+    id: 1,
+    nome: 'Luís'
+  },
+  {
+    id: 2,
+    nome: 'Luís'
+  },
+  {
+    id: 3,
+    nome: 'Maria'
+  },
+  {
+    id: 4,
+    nome: 'José'
   }
-  componentDidUpdate(prevProps, prevState) {
-    console.log(
-      prevProps.resultado,
-      this.props.resultado
-    );
-  }
-}
+];
 
-class Contador extends React.Component {
-  state = {
-    valor: 0
-  };
-  componentDidMount() {
-    this.timerID = setInterval(() => {
-      this.setState(s => ({
-        valor: s.valor + 1
-      }));
-    }, 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-  render() {
-    return (
-      <div>
-        <Resultado resultado={this.state.valor} />
-      </div>
-    );
-  }
-}
-class App extends React.Component {
-  state = {
-    mostrarContador: true
-  };
-  toggleMostrador = () => {
-    this.setState(s => ({
-      mostrarContador: !s.mostrarContador
-    }));
-  };
-  render() {
-    if (!this.state.mostrarContador) {
-      return (
-        <div>
-          <input
-            type="checkbox"
-            checked={this.state.mostrarContador}
-            onChange={this.toggleMostrador}
-          />
-        </div>
-      );
-    }
-    return (
-      <div>
-        <input
-          type="checkbox"
-          checked={this.state.mostrarContador}
-          onChange={this.toggleMostrador}
-        />
-        <Contador />
-      </div>
-    );
-  }
-}
+const Pessoa = p => (
+  <li>
+    <strong>{p.id}</strong> - {p.nome}
+  </li>
+);
+
+const App = () => (
+  <ul>
+    {pessoas.map(p => (
+      <Pessoa
+        key={p.id.toString()}
+        id={p.id}
+        nome={p.nome}
+      />
+    ))}
+  </ul>
+);
 
 ReactDOM.render(
   <App />,
