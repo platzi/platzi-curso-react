d23d38b16005f99d4d294f9eb3d78c26b1d0b16e
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-05 16:18:11 -0500

    Aula 17

--- src/index.js
+++ src/index.js
@@ -1,70 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const useHackerNewsApi = () => {
-  const [query, setQuery] = React.useState('');
-  const [items, setItems] = React.useState([]);
-  const [loading, setLoading] = React.useState(
-    false
-  );
-  React.useEffect(() => {
-    if (!query) {
-      return;
-    }
-    const search = async () => {
-      setLoading(true);
-      const result = await fetch(
-        `https://hn.algolia.com/api/v1/search_by_date?query=${query}&tags=story&numericFilters=created_at_i%3E2019-01-01`
-      );
-      const data = await result.json();
-      setItems(data.hits);
-      setLoading(false);
-    };
-    search();
-  }, [query]);
-  return {
-    setQuery,
-    items,
-    loading
-  };
-};
-
 const App = () => {
-  const [
-    searchValue,
-    setSearchValue
-  ] = React.useState('');
-  const {
-    setQuery,
-    items,
-    loading
-  } = useHackerNewsApi();
+  const [contador, setContador] = React.useState(
+    0
+  );
+
+  const altereContador = operacao =>
+    setContador(c => operacao(c));
 
   return (
     <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          setQuery(searchValue);
+      <button
+        onClick={() => setContador(c => c + 1)}
+      >
+        adiciona
+      </button>
+      <div
+        onMouseEnter={() =>
+          altereContador(c => c - 1)
+        }
+        style={{
+          backgroundColor: 'red',
+          width: 50,
+          height: 50,
+          marginTop: 10
         }}
       >
-        <input
-          type="text"
-          value={searchValue}
-          onChange={e =>
-            setSearchValue(e.target.value)
-          }
-        />
-        <button type="submit">Pesquisar</button>
-      </form>
-      {loading && <h1>Carregando</h1>}
-      {!loading && items.length > 0 && (
-        <ul>
-          {items.map(i => (
-            <li key={i.objectID}>{i.title}</li>
-          ))}
-        </ul>
-      )}
+        subtrai
+      </div>
+      <h1>{contador}</h1>
     </div>
   );
 };
