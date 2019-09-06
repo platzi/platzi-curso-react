7e1bb6ce86158c160d09de3e108630a73687f1be
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-06 15:19:10 -0500

    aula23

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const IdiomaContext = React.createContext();
+
+const Ola = () => {
+  const ctx = React.useContext(IdiomaContext);
+  return (
+    <h1>
+      {ctx.idioma === 'pt-BR' ? 'Olá' : 'Hello'}
+    </h1>
+  );
+};
+const MudaIdioma = () => {
+  const ctx = React.useContext(IdiomaContext);
+  return (
+    <div>
+      <button
+        onClick={() => ctx.setIdioma('pt-BR')}
+      >
+        português
+      </button>
+      <button onClick={() => ctx.setIdioma('en')}>
+        inglês
+      </button>
+    </div>
+  );
+};
 const App = props => {
-  return <div>oi</div>;
+  const [idioma, setIdioma] = React.useState(
+    'pt-BR'
+  );
+  return (
+    <IdiomaContext.Provider
+      value={{ idioma, setIdioma }}
+    >
+      <div>
+        <Ola />
+        <MudaIdioma />
+      </div>
+    </IdiomaContext.Provider>
+  );
 };
 
 ReactDOM.render(
