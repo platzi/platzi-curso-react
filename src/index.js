35be558d80249d2238d8eb8a6ae1fbc489b03839
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-06 14:47:37 -0500

    Aula21

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const focaNoElemento = elementoHtmlDeVerdade =>
-  elementoHtmlDeVerdade.focus();
-
 const App = props => {
-  const input1 = React.useRef(null);
-  const input2 = React.useRef(null);
-
   return (
-    <div>
-      <button
-        onClick={() =>
-          focaNoElemento(input1.current)
-        }
-      >
-        Foco no 1
-      </button>
-      <button
-        onClick={() =>
-          focaNoElemento(input2.current)
-        }
-      >
-        Foco no 2
-      </button>
-      <input type="text" ref={input1} />
-      <input type="text" ref={input2} />
-    </div>
+    <>
+      <div>oi1</div>
+      <div>oi2</div>
+    </>
   );
 };
 
