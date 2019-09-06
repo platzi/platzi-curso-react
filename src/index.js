bb5ed5e36b5a33f7b7b2239bb3e6b2afb229f3ac
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-06 15:08:49 -0500

    reset

--- src/index.js
+++ src/index.js
@@ -1,50 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const COTACAO = 4;
-const convertaDolarEmReal = dolar =>
-  dolar * COTACAO;
-const convertaRealEmDolar = real =>
-  real / COTACAO;
-
-const InputDeValor = ({
-  moeda,
-  valor,
-  onChange
-}) => {
-  return (
-    <div>
-      <label>{moeda}</label>
-      <input
-        type="number"
-        value={valor || ''}
-        onChange={e => onChange(e.target.value)}
-      />
-    </div>
-  );
-};
-
 const App = props => {
-  const [
-    valorEmDolar,
-    setValorEmDolar
-  ] = React.useState();
-  return (
-    <div>
-      <InputDeValor
-        moeda="R$"
-        valor={convertaDolarEmReal(valorEmDolar)}
-        onChange={v =>
-          setValorEmDolar(convertaRealEmDolar(v))
-        }
-      />
-      <InputDeValor
-        moeda="US$"
-        valor={valorEmDolar}
-        onChange={v => setValorEmDolar(v)}
-      />
-    </div>
-  );
+  return <div>oi</div>;
 };
 
 ReactDOM.render(
