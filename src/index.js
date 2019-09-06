9bb8cfc7d092efd60c12a87796a10cd951138b16
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-06 14:59:46 -0500

    aula22

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const COTACAO = 4;
+const convertaDolarEmReal = dolar =>
+  dolar * COTACAO;
+const convertaRealEmDolar = real =>
+  real / COTACAO;
+
+const InputDeValor = ({
+  moeda,
+  valor,
+  onChange
+}) => {
+  return (
+    <div>
+      <label>{moeda}</label>
+      <input
+        type="number"
+        value={valor || ''}
+        onChange={e => onChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
 const App = props => {
-  return <div>oi</div>;
+  const [
+    valorEmDolar,
+    setValorEmDolar
+  ] = React.useState();
+  return (
+    <div>
+      <InputDeValor
+        moeda="R$"
+        valor={convertaDolarEmReal(valorEmDolar)}
+        onChange={v =>
+          setValorEmDolar(convertaRealEmDolar(v))
+        }
+      />
+      <InputDeValor
+        moeda="US$"
+        valor={valorEmDolar}
+        onChange={v => setValorEmDolar(v)}
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(
